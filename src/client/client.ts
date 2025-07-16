a1472e9feccf9b04cdfe0c483366842e0f35b5c9
Bilal Yasin <bilalyasin1616@gmail.com>
2025-07-16 11:26:36 +0500

    feat: added package files

---
+++ src/client/client.ts
@@ -0,0 +1,14 @@
+import { API_OPERATION_TYPE_MAP } from '../api';
+import { ClientConfiguration } from './config';
+import { createApiInstance } from './create-api-instance';
+
+export class TikTokShopNodeApiClient {
+  readonly config?: ClientConfiguration;
+  readonly api: API_OPERATION_TYPE_MAP;
+
+  constructor({ config }: { config?: ClientConfiguration }) {
+    this.config = config;
+    const { api } = createApiInstance(this);
+    this.api = api;
+  }
+}
